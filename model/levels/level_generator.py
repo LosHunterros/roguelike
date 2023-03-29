3e892d81931b8a6a0490307b5b6d061ada66c301
Kaszydo <kaszydo@gmail.com>
2023-03-29 23:24:57 +0200

    fixed error with player starting in the wall

--- model/levels/level_generator.py
+++ model/levels/level_generator.py
@@ -1,6 +1,7 @@
 from config import *
 import numpy as np
 from random import randint, choice
+import controller.level_controller as lc
 
 
 class Game_Map:
@@ -18,37 +19,103 @@ class Game_Map:
             self.grass = grass
             self.steps = steps
 
+        def replant_trees(self, board, x, y):
+            place_for_tree = [
+                board[x, y] == PATH,
+                board[x - 1, y] == PATH,
+                board[x, y - 1] == PATH,
+                board[x, y + 1] == PATH,
+                board[x + 1, y] == PATH,
+                board[x - 2, y] == PATH,
+                board[x - 2, y - 1] == PATH,
+                board[x - 2, y + 1] == PATH,
+                board[x - 1, y - 2] == PATH,
+                board[x - 1, y - 1] == PATH,
+                board[x - 1, y + 2] == PATH,
+                board[x - 1, y + 1] == PATH,
+                board[x, y - 2] == PATH,
+                board[x, y + 2] == PATH,
+                board[x + 1, y - 2] == PATH,
+                board[x + 1, y - 1] == PATH,
+                board[x + 1, y + 1] == PATH,
+                board[x + 1, y + 2] == PATH,
+                board[x + 2, y - 1] == PATH,
+                board[x + 2, y] == PATH,
+                board[x + 2, y + 1] == PATH,
+            ]
+            if all(place_for_tree):
+                board[x, y] = WALL
+                board[x - 1, y] = WALL
+                board[x, y - 1] = WALL
+                board[x, y + 1] = WALL
+                board[x + 1, y] = WALL
+                return board, True
+            return board, False
+
         def drunk_troll(self, x: int, y: int, board: list):
-            number_of_trees_to_leave = int(np.count_nonzero(board == WALL)*0.4)
+            number_of_trees_to_leave = int(np.count_nonzero(board == WALL) * 0.3)
             board[x, y] = self.grass
-            board[x, y+1] = self.grass
-            board[x+1, y] = self.grass
-            board[x+1, y+1] = self.grass
-            posible_direction = {
-                'w': [x-1, y], 's': [x+1, y], 'a': [x, y-1], 'd': [x, y+1]}
+            y_conditions = [
+                [y + 1 > 1, y + 1 < len(board[0]) - 2],
+                [y - 1 > 1, y - 1 < len(board[0]) - 2],
+            ]
+            x_conditions = [
+                [x + 1 > 1, x + 1 < len(board) - 2],
+                [x - 1 > 1, x - 1 < len(board) - 2],
+            ]
+            if all(y_conditions[0]):
+                board[x, y + 1] = self.grass
+                if all(x_conditions[0]):
+                    board[x + 1, y + 1] = self.grass
+                    if all(x_conditions[1]):
+                        board[x - 1, y + 1] = self.grass
+            if all(y_conditions[1]):
+                board[x, y - 1] = self.grass
+                if all(x_conditions[0]):
+                    board[x + 1, y - 1] = self.grass
+                    if all(x_conditions[1]):
+                        board[x - 1, y - 1] = self.grass
             while np.count_nonzero(board == WALL) > number_of_trees_to_leave:
-                steps_to_make = self.steps
-                while steps_to_make > 0:
-                    next_x = 0
-                    next_y = 0
-                    while next_x < 1 or next_x > len(board)-2 or next_y < 1 or next_y > len(board[0])-2:
-                        direction = choice(list(posible_direction.items()))
-                        next_x, next_y = direction[1]
-                    for _ in range(3):
-                        board[x, y] = self.grass
-                        x, y = next_x, next_y
-                        posible_direction = {
-                            'w': [x-1, y], 's': [x+1, y], 'a': [x, y-1], 'd': [x, y+1]}
-                        next_x, next_y = posible_direction[direction[0]]
-                        while next_x < 1 or next_x > len(board)-2 or next_y < 1 or next_y > len(board[0])-2:
-                            direction = choice(list(posible_direction.items()))
-                            next_x, next_y = direction[1]
-                        steps_to_make -= 1
-                x = randint(1, len(board)-2)
-                y = randint(1, len(board[0])-2)
-                while board[x, y] == self.tree:
-                    x = randint(1, len(board)-2)
-                    y = randint(1, len(board[0])-2)
+                for _ in range(self.steps):
+                    board[x, y] = self.grass
+                    available_direction = []
+                    posible_direction = {
+                        "w": [x - 1, y],
+                        "s": [x + 1, y],
+                        "a": [x, y - 1],
+                        "d": [x, y + 1],
+                    }
+                    for key in posible_direction.keys():
+                        try:
+                            board[posible_direction[key]]
+                        except:
+                            pass
+                        conditions = [
+                            posible_direction[key][0] > 1,
+                            posible_direction[key][0] < len(board) - 2,
+                            posible_direction[key][1] > 1,
+                            posible_direction[key][1] < len(board[0]) - 2,
+                        ]
+                        if all(conditions):
+                            available_direction.append(posible_direction[key])
+                    try:
+                        direction = choice(available_direction)
+                        x, y = direction
+                    except IndexError:
+                        x = randint(1, len(board) - 2)
+                        y = randint(1, len(board[0]) - 2)
+                    available_direction.clear()
+            i = 0
+            j = 0
+            while i < 15 or j < 30:
+                x, y = None, None
+                while x == None and y == None:
+                    x, y = lc.get_random_path(board)
+                board, tree_was_planted = self.replant_trees(board, x, y)
+                if tree_was_planted:
+                    i += 1
+                j += 1
+
             return board
 
     #
--- roguelike.py
+++ roguelike.py
@@ -9,5 +9,5 @@ def main():
     level.level()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
